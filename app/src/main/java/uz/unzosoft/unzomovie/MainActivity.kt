ee96467f00594edccc19b501b6aa59705d23e654
AbdurashidovShahzod <shahzod9933@gmail.com>
2021-05-05 14:26:32 +0500

    Added view binding

--- app/src/main/java/uz/unzosoft/unzomovie/MainActivity.kt
+++ app/src/main/java/uz/unzosoft/unzomovie/MainActivity.kt
@@ -2,10 +2,13 @@ package uz.unzosoft.unzomovie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import uz.unzosoft.unzomovie.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/uz/unzosoft/unzomovie/ui/movie/MovieFragment.kt
+++ app/src/main/java/uz/unzosoft/unzomovie/ui/movie/MovieFragment.kt
@@ -1,4 +1,4 @@
-package uz.unzosoft.unzomovie.ui.details
+package uz.unzosoft.unzomovie.ui.movie
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,16 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import uz.unzosoft.unzomovie.R
-import uz.unzosoft.unzomovie.databinding.FragmentDetailsBinding
+import uz.unzosoft.unzomovie.databinding.FragmentMovieBinding
 
+class MovieFragment : Fragment() {
 
-class DetailsFragment : Fragment() {
-    lateinit var binding: FragmentDetailsBinding
+    lateinit var binding: FragmentMovieBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentDetailsBinding.inflate(layoutInflater)
+        binding = FragmentMovieBinding.inflate(layoutInflater)
         return binding.root
     }
 
