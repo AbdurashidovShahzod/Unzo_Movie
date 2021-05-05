5f73470d254c52516ce8bb6c9467172ca8655d9d
AbdurashidovShahzod <shahzod9933@gmail.com>
2021-05-05 17:46:06 +0500

    Retrofit 2 added and movie app keys

---
+++ app/src/main/java/uz/unzosoft/unzomovie/utils/Constant.kt
@@ -0,0 +1,6 @@
+package uz.unzosoft.unzomovie.utils
+
+object Constants {
+    const val BASE_URL = "http://www.omdbapi.com/"
+//    const val BASE_URL = "http://www.omdbapi.com/?s=Lucifer&page=6&apikey=2c20f2cc"
+}
