9e30f86cc1e7fdbff0abedbc75d9f0b290cf3f42
AbdurashidovShahzod <shahzod9933@gmail.com>
2021-05-06 01:15:20 +0500

    DataBinding

--- app/src/main/java/uz/unzosoft/unzomovie/utils/BindingAdapters.kt
+++ app/src/main/java/uz/unzosoft/unzomovie/utils/BindingAdapters.kt
@@ -1,5 +1,13 @@
 package uz.unzosoft.unzomovie.utils
 
-class BindingAdapters {
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
 
+@BindingAdapter("load")
+fun loadImage(view: ImageView, url: String) {
+    Glide.with(view)
+        .load(url)
+        .into(view)
 }
+
